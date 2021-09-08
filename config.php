9d61c4ccdcfe89610a87c9f9af37671d13ff10fb
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-09-08 19:08:40 +0200

    'cover' added and sample images filled in

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
 $config['title'] = 'yourTitle'; /* Title of your (Imagehost)-website */
 $config['fontawesome-key'] = 'yourKey'; /* Your fontAwesome key, can be found here: https://fontawesome.com/kits */
-$config['faviconPath'] = "yourFaviconPath"; /* Path to the favicon of your website, file must be named 'favicon.ico' */
-$config['logoPath'] = "yourLogoPath"; /* Path to the logo of your website */
-$config ['bannerPath'] = 'yourBannerPath'; /* Path to the banner of your website */
+$config['faviconPath'] = "images/favicon.ico"; /* Path to the favicon of your website, file must be named 'favicon.ico' */
+$config['logoPath'] = "images/logo.png"; /* Path to the logo of your website */
+$config['bannerPath'] = 'images/banner.png'; /* Path to the banner of your website */
+$config['coverPath'] = 'images/cover.png'; /* Path to the cover of your website */
 $config['domain'] = 'example.com'; /* Domain of your website */
 $config['description'] = 'yourDescription'; /* Small description that will be shown on the site */
 $config['maxFileSize'] = 'yourMaxFileSize'; /* The max file-size users can upload (in bytes) */
