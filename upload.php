1f6def47fec21fdff01f1adf132e08bcd4b446d5
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-09-08 19:10:40 +0200

    Added the cover of the config file

--- upload.php
+++ upload.php
@@ -11,7 +11,7 @@ include_once 'config.php'
     <meta charset="utf-8">
 
     <title>
-        <?= $config['title']; ?> 
+        <?= $config['title']; ?>
     </title>
 
     <script src="https://kit.fontawesome.com/<?= $config['fontawesome-key']; ?>.js" crossorigin="anonymous"></script>
@@ -20,13 +20,35 @@ include_once 'config.php'
 
 </head>
 
+<style>
+    .popup {
+        background-image: url(<?= $config['coverPath'] ?>);
+    }
+</style>
+
 <body>
 
     <img src="<?= $config['logoPath']; ?>" alt="logo" class="logo">
 
     <div class="popup">
         <form action="file-uploader.php" method="POST" enctype="multipart/form-data">
-            <btn class="close-btn" onclick="window.location.href='home.php'"><svg width="88" height="88" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_d)"><circle cx="44" cy="36" r="20" fill="#3C424B" /></g><path fill-rule="evenodd" clip-rule="evenodd" d="M39.121 29.828a1.04 1.04 0 0 0-1.414 0c-.39.37-.39.97 0 1.34l4.95 4.687-4.95 4.688c-.39.37-.39.969 0 1.339s1.024.37 1.414 0l4.95-4.688 4.95 4.688c.39.37 1.023.37 1.414 0 .39-.37.39-.97 0-1.34l-4.95-4.687 4.95-4.688a.912.912 0 0 0 0-1.339 1.04 1.04 0 0 0-1.414 0l-4.95 4.688-4.95-4.688z" fill="#fff" /><defs><filter id="filter0_d" x="0" y="0" width="88" height="88" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix" /><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" /><feOffset dy="8" /><feGaussianBlur stdDeviation="12" /><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.56 0" /><feBlend in2="BackgroundImageFix" result="effect1_dropShadow" /><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape" /></filter></defs></svg></btn>
+            <btn class="close-btn" onclick="window.location.href='home.php'"><svg width="88" height="88" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g filter="url(#filter0_d)">
+                        <circle cx="44" cy="36" r="20" fill="#3C424B" />
+                    </g>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M39.121 29.828a1.04 1.04 0 0 0-1.414 0c-.39.37-.39.97 0 1.34l4.95 4.687-4.95 4.688c-.39.37-.39.969 0 1.339s1.024.37 1.414 0l4.95-4.688 4.95 4.688c.39.37 1.023.37 1.414 0 .39-.37.39-.97 0-1.34l-4.95-4.687 4.95-4.688a.912.912 0 0 0 0-1.339 1.04 1.04 0 0 0-1.414 0l-4.95 4.688-4.95-4.688z" fill="#fff" />
+                    <defs>
+                        <filter id="filter0_d" x="0" y="0" width="88" height="88" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                            <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+                            <feOffset dy="8" />
+                            <feGaussianBlur stdDeviation="12" />
+                            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.56 0" />
+                            <feBlend in2="BackgroundImageFix" result="effect1_dropShadow" />
+                            <feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+                        </filter>
+                    </defs>
+                </svg></btn>
             <label for="file-upload" class="file-btn">
                 <i class="fa fa-cloud-upload"></i> Upload
             </label>
