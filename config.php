c9d40d398d468648d20720b0744821833e4d4713
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-10-03 09:09:08 +0200

    Added url

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-$config['title'] = 'yourTitle'; /* Title of your (Imagehost)-website */
-$config['fontawesome-key'] = 'yourKey'; /* Your fontAwesome key, can be found here: https://fontawesome.com/kits */
+$config['title'] = 'Imagehost-Template'; /* Title of your (Imagehost)-website */
+$config['fontawesome-key'] = '466b8bcbcf'; /* Your fontAwesome key, can be found here: https://fontawesome.com/kits */
 $config['faviconPath'] = "images/favicon.ico"; /* Path to the favicon of your website, file must be named 'favicon.ico' */
 $config['logoPath'] = "images/logo.png"; /* Path to the logo of your website */
 $config['bannerPath'] = 'images/banner.png'; /* Path to the banner of your website */
 $config['coverPath'] = 'images/cover.png'; /* Path to the cover of your website */
-$config['domain'] = 'example.com'; /* Domain of your website */
-$config['description'] = 'yourDescription'; /* Small description that will be shown on the site */
-$config['maxFileSize'] = 'yourMaxFileSize'; /* The max file-size users can upload (in bytes) */
+$config['description'] = 'The free-to-use template for your Imagehost-website made with PHP, HTML and CSS!'; /* Small description that will be shown on the site */
+$config['maxFileSize'] = '500000000000000000000'; /* The max file-size users can upload (in bytes) */
+$config['url'] = 'https://www.example.com/';
