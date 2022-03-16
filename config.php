066018fb13fa0b56b58e7d160651812415ecd163
Ilian <72648901+Iliannnn@users.noreply.github.com>
2022-03-16 17:29:29 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-$config['title'] = 'Imagehost-Template'; /* Title of your (Imagehost)-website */
+$config['title'] = 'Imagehost'; /* Title of your website */
 $config['fontawesome-key'] = 'yourKey'; /* Your fontAwesome key, can be found here: https://fontawesome.com/kits */
 $config['faviconPath'] = "images/favicon.ico"; /* Path to the favicon of your website, file must be named 'favicon.ico' */
 $config['logoPath'] = "images/logo.png"; /* Path to the logo of your website */
 $config['bannerPath'] = 'images/banner.png'; /* Path to the banner of your website */
 $config['coverPath'] = 'images/cover.png'; /* Path to the cover of your website */
-$config['description'] = 'The free-to-use template for your Imagehost-website made with PHP, HTML and CSS!'; /* Small description that will be shown on the site */
+$config['description'] = 'The free-to-use template for your Imagehost-website made with PHP'; /* Small description that will be shown on the site */
 $config['maxFileSize'] = '500000000000000000000'; /* The max file-size users can upload (in bytes) */
 $config['url'] = 'https://www.example.com';
